f0cefef93957248387d22afea9806d74c34e1778
MakalaMabotja <138364127+MakalaMabotja@users.noreply.github.com>
2024-08-14 20:12:46 +0200

    Change of ownership for investments

--- transactions/invest_transact.py
+++ transactions/invest_transact.py
@@ -1,7 +1,8 @@
 def invest(player, investment):
     if player.balance >= investment.price:
         player.balance -= investment.price
-        investment.owner == player
+        investment.change_owner(player)
+        # investment.owner == player
         player.properties.append(investment)
         
 def earn_money(player, investment):
